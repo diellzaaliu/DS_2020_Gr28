85bdb23565c2e2345546bb592851469f3e07646c
AferditaB <58392109+AferditaB@users.noreply.github.com>
2020-03-28 21:46:03 +0100

    Update PlayfairCipher.java

--- PlayfairCipher.java
+++ PlayfairCipher.java
@@ -25,3 +25,69 @@ public class PlayfairCipher
         }
         KeyWord = K_adjust;
     }
+public void KeyGen()
+    {
+        boolean flag = true;
+        char current;
+        Key = KeyWord;
+        for (int i = 0; i < 26; i++)
+        {
+            current = (char) (i + 97);
+            if (current == 'j')
+                continue;
+            for (int j = 0; j < KeyWord.length(); j++)
+            {
+                if (current == KeyWord.charAt(j))
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag)
+                Key = Key + current;
+            flag = true;
+        }
+        System.out.println(Key);
+        matrix();
+    }
+ 
+    private void matrix()
+    {
+        int counter = 0;
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                matrix_array[i][j] = Key.charAt(counter);
+                System.out.print(matrix_array[i][j] + " ");
+                counter++;
+            }
+            System.out.println();
+        }
+    }
+ 
+    private String format(String old_text)
+    {
+        int i = 0;
+        int len = 0;
+        String text = new String();
+        len = old_text.length();
+        for (int tmp = 0; tmp < len; tmp++)
+        {
+            if (old_text.charAt(tmp) == 'j')
+            {
+                text = text + 'i';
+            }
+            else
+                text = text + old_text.charAt(tmp);
+        }
+        len = text.length();
+        for (i = 0; i < len; i+=2)
+        {
+            if (text.charAt(i + 1) == text.charAt(i))
+            {
+                text = text.substring(0, i + 1) + 'x' + text.substring(i + 1);
+            }
+        }
+        return text;
+    }
