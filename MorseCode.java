0e7ff3311a6d0f5ac05afb439f77fb912620c587
selimi1 <44116298+selimi1@users.noreply.github.com>
2020-03-28 12:11:13 +0100

    Create MorseCode.java

---
+++ MorseCode.java
@@ -0,0 +1,61 @@
+public class MorseCode{
+    
+	static String morseDecode(String s){ 
+	
+        switch (s){ 
+            case ".-": 
+                return "a"; 
+            case "-...": 
+                return "b"; 
+            case "-.-.": 
+                return "c"; 
+            case "-..": 
+                return "d"; 
+            case ".": 
+                return "e"; 
+            case "..-.": 
+                return "f"; 
+            case "--.": 
+                return "g"; 
+            case "....": 
+                return "h"; 
+            case "..": 
+                return "i"; 
+            case ".---": 
+                return "j"; 
+            case "-.-": 
+                return "k"; 
+            case ".-..": 
+                return "l"; 
+            case "--": 
+                return "m"; 
+            case "-.": 
+                return "n"; 
+            case "---": 
+                return "o"; 
+            case ".--.": 
+                return "p"; 
+            case "--.-": 
+                return "q"; 
+            case ".-.": 
+                return "r"; 
+            case "...": 
+                return "s"; 
+            case "-": 
+                return "t"; 
+            case "..-": 
+                return "u"; 
+            case "...-": 
+                return "v"; 
+            case ".--": 
+                return "w"; 
+            case "-..-": 
+                return "x"; 
+            case "-.--": 
+                return "y";  
+            case "--..": 
+                return "z"; 
+        } 
+        return ""; 
+    } 
+	
