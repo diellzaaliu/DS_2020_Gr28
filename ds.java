baca7beae0191150202f4ecfaa5804ca2a5861db
diellzaaliu <58387674+diellzaaliu@users.noreply.github.com>
2020-03-29 15:44:43 +0200

    Create ds.java

---
+++ ds.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class ds{
+
+    public static void main(String[] args)
+    {
+        PlayfairCipher x = new PlayfairCipher();
+		
+	 if(args.length == 0)
+    {
+        System.out.println("Proper Usage is: PlayFair encrypt/decrypt <key> <plaintext>\nor\nMorseCode encode/decode <text>\nor\nBeale encrypt/decrypt <book> <plaintext> ");
+        System.exit(1);
+    }
+       
+		else if(args[0].equalsIgnoreCase("PlayFair")){
+   
+        x.setKey(args[2]);
+        x.KeyGen();
+		
+			if(args[1].equalsIgnoreCase("Encrypt")){
+				if (args[3].length() % 2 == 0){
+					System.out.println("Encryption: " + x.encryptMessage(args[3]));
+			}
+			}
+			else if(args[1].equalsIgnoreCase("Decrypt")){
+				if (args[3].length() % 2 == 0){
+					System.out.println("Decryption: " + x.decryptMessage(args[3]));
+			}
+			}
+			else{
+					System.out.println("Message length should be even");
+				}
+				
+		}
+		
+		else if(args[0].equalsIgnoreCase("MorseCode")){
+			
+			if(args[1].equalsIgnoreCase("Encode")){
+				MorseCode.Encode(args[2]);
+			}
+			else if(args[1].equalsIgnoreCase("Decode")){
+				MorseCode.Decode(args[2]);
+			}
+		}
+		else if(args[0].equalsIgnoreCase("Beale")){
+			BealeCipher text=new BealeCipher();
+			text.ReadFile(args[2]);
+			if(args[1].equalsIgnoreCase("Encrypt")){
+				text.Encryption(args[3]);
+			}else if(args[1].equalsIgnoreCase("Decrypt")){
+				text.Decryption(args[3]);
+			}
+		}
+		else{
+			System.out.println("Proper Usage is: PlayFair encrypt <key> <plaintext>\nor\nMorseCode encode <text>\nor\nBeale encrypt <book> <plaintext> ");
+			System.exit(1);
+		}
+				
+	}
+	}	
+    
+	
+
+ 
