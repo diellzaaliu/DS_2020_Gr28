ae452109fa52d7afbda0b2ef5f105b89cfd8456d
selimi1 <44116298+selimi1@users.noreply.github.com>
2020-03-29 13:12:49 +0200

    Update MorseCode.java

--- MorseCode.java
+++ MorseCode.java
@@ -58,4 +58,77 @@ public class MorseCode{
         } 
         return ""; 
     } 
+	static String morseEncode(char x){ 
 	
+        switch (x){ 
+            case 'a': 
+                return ".- "; 
+            case 'b': 
+                return "-... "; 
+            case 'c': 
+                return "-.-. "; 
+            case 'd': 
+                return "-.. "; 
+            case 'e': 
+                return ". "; 
+            case 'f': 
+                return "..-. "; 
+            case 'g': 
+                return "--. "; 
+            case 'h': 
+                return ".... "; 
+            case 'i': 
+                return ".. "; 
+            case 'j': 
+                return ".--- "; 
+            case 'k': 
+                return "-.- "; 
+            case 'l': 
+                return ".-.. "; 
+            case 'm': 
+                return "-- "; 
+            case 'n': 
+                return "-. "; 
+            case 'o': 
+                return "--- "; 
+            case 'p': 
+                return ".--. "; 
+            case 'q': 
+                return "--.- "; 
+            case 'r': 
+                return ".-. "; 
+            case 's': 
+                return "... "; 
+            case 't': 
+                return "- "; 
+            case 'u': 
+                return "..- "; 
+            case 'v': 
+                return "...- "; 
+            case 'w': 
+                return ".-- "; 
+            case 'x': 
+                return "-..- "; 
+            case 'y': 
+                return "-.-- "; 
+          
+            case 'z': 
+                return "--.. "; 
+        } 
+        return ""; 
+    } 
+      
+    static void Encode(String s){ 
+       
+        for (int i = 0;i<s.length(); i++) 
+            System.out.print(MorseCode.morseEncode(s.charAt(i))); 
+            System.out.println(); 
+    } 
+	static void Decode(String s){ 
+       
+	   String str[]=s.split("[ ]+");
+        for (int i = 0;i<str.length; i++) 
+            System.out.print(MorseCode.morseDecode(str[i])); 
+            System.out.println(); 
+    }
+}
