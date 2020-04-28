b0a4ffb40d0de253a7e6b622d114f275ce3b67b9
Diellza Aliu <DiellzaAliu99@gmail.com>
2020-04-28 14:59:19 +0200

    Updated

--- CreateDeleteUser.java
+++ CreateDeleteUser.java
@@ -21,30 +21,26 @@ public class CreateDeleteUser{
 		return keypair;
 
     }
-	public void createUser(String useri) throws Exception{
-
+	public void createUser(String user) throws Exception{
+		
+		String useri = user.trim();
         try {
 			
-			if (useri == null || useri.trim().isEmpty()) {
-			System.out.println("Incorrect format of string");
-			System.exit(1);
-			}
-			Pattern p = Pattern.compile("[^A-Za-z0-9]");
+			Pattern p = Pattern.compile("[^A-Za-z0-9]"); //referenced by www.stackoverflow.com
 			Matcher m = p.matcher(useri);
 			boolean b = m.find();
 			if (b){
-			System.out.println("It is not allowed to use special characters");
-			System.exit(1);
-			
+				System.out.println("It is not allowed to use special characters");
+				System.exit(1);
 			}
             BufferedWriter writer = new BufferedWriter(new FileWriter("C:/Users/Twin/Desktop/FIEK/SEMESTRI IV/Siguria e te dhenave/Projekti/Keys/"+useri+".public.xml"));
 			publicKey= Base64.getMimeEncoder().encodeToString( keypair.getPublic().getEncoded());
             writer.write("------Public Key-------\n"+publicKey);
-			System.out.println("Eshte krijuar celesi publik '"+useri+".public.xml'");
+			System.out.println("Eshte krijuar celesi publik '"+useri.trim()+".public.xml'");
 			
 			BufferedWriter writer1 = new BufferedWriter(new FileWriter("C:/Users/Twin/Desktop/FIEK/SEMESTRI IV/Siguria e te dhenave/Projekti/Keys/"+useri+".private.xml"));
 			privateKey=Base64.getMimeEncoder().encodeToString( keypair.getPrivate().getEncoded());
-			System.out.println("Eshte krijuar celesi publik '"+useri+".public.xml'");
+			System.out.println("Eshte krijuar celesi publik '"+useri.trim()+".public.xml'");
             writer1.write("------Private Key-------\n"+privateKey);
 			
             writer.close();
