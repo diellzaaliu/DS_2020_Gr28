c36f91ccbb2765658df9aec691f5ebe725ee4f3b
Diellza Aliu <DiellzaAliu99@gmail.com>
2020-05-02 23:16:34 +0200

    updated

--- CreateDeleteUser.java
+++ CreateDeleteUser.java
@@ -23,7 +23,7 @@ public class CreateDeleteUser{
 	public void createUser(String user) throws Exception{
 		
 		String useri = user.trim();
-		if(Files.exists(Paths.get("C:/Users/Twin/Desktop/FIEK/SEMESTRI IV/Siguria e te dhenave/Projekti/Keys/"+useri+".public.xml"))){
+		if(Files.exists(Paths.get(useri+".public.xml"))){
 			System.out.println("Gabim: Celesi '"+useri+"' ekziston paraprakisht.");
 			System.exit(1);
 		}
@@ -33,15 +33,15 @@ public class CreateDeleteUser{
 			Matcher m = p.matcher(useri);
 			boolean b = m.find();
 			if (b){
-				System.out.println("It is not allowed to use special characters");
+				System.out.println("Nuk jane te lejuara karakteret speciale.");
 				System.exit(1);
 			}
-            BufferedWriter writer = new BufferedWriter(new FileWriter("C:/Users/Twin/Desktop/FIEK/SEMESTRI IV/Siguria e te dhenave/Projekti/Keys/"+useri+".public.xml"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(useri+".public.xml"));
 			publicKey= Base64.getMimeEncoder().encodeToString( keypair.getPublic().getEncoded());
             writer.write("------Public Key-------\n"+publicKey);
 			System.out.println("Eshte krijuar celesi publik '"+useri+".public.xml'");
 			
-			BufferedWriter writer1 = new BufferedWriter(new FileWriter("C:/Users/Twin/Desktop/FIEK/SEMESTRI IV/Siguria e te dhenave/Projekti/Keys/"+useri+".private.xml"));
+			BufferedWriter writer1 = new BufferedWriter(new FileWriter(useri+".private.xml"));
 			privateKey=Base64.getMimeEncoder().encodeToString( keypair.getPrivate().getEncoded());
 			System.out.println("Eshte krijuar celesi private '"+useri+".private.xml'");
             writer1.write("------Private Key-------\n"+privateKey);
@@ -56,13 +56,13 @@ public class CreateDeleteUser{
 	public void deleteUser(String shfrytezuesi) throws Exception{
 		shfrytezuesi = shfrytezuesi.trim();
 		
-		if(!Files.exists(Paths.get("C:/Users/Twin/Desktop/FIEK/SEMESTRI IV/Siguria e te dhenave/Projekti/Keys/"+shfrytezuesi+".public.xml"))){
+		if(!Files.exists(Paths.get(shfrytezuesi+".public.xml"))){
 			System.out.println("Gabim: Celesi '"+shfrytezuesi+"' nuk ekziston. ");
 			System.exit(1);
 		}
 		else{
-            Files.deleteIfExists(Paths.get("C:/Users/Twin/Desktop/FIEK/SEMESTRI IV/Siguria e te dhenave/Projekti/Keys/"+shfrytezuesi+".public.xml")); 
-            Files.deleteIfExists(Paths.get("C:/Users/Twin/Desktop/FIEK/SEMESTRI IV/Siguria e te dhenave/Projekti/Keys/"+shfrytezuesi+".private.xml")); 
+            Files.deleteIfExists(Paths.get(shfrytezuesi+".public.xml")); 
+            Files.deleteIfExists(Paths.get(shfrytezuesi+".private.xml")); 
 			System.out.println("Eshte larguar celesi privat '"+shfrytezuesi+".private.xml'");
 			System.out.println("Eshte larguar celesi publik '"+shfrytezuesi+".public.xml'");
 		}
