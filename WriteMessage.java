91d100b0c46cdd08ca8960b8ea6bdb4c0f923793
AgnesaS <44116298+AgnesaS@users.noreply.github.com>
2020-06-07 23:31:37 +0200

    Update WriteMessage.java

--- WriteMessage.java
+++ WriteMessage.java
@@ -1,1 +1,45 @@
 
+import java.util.*; 
+import javax.mail.*; 
+import javax.mail.internet.*; 
+import javax.activation.*; 
+  
+public class WriteMessage { 
+      
+    public static void main(String[] args) 
+    { 
+       
+        String to = "agnesa.selimi2@gmail.com";  
+        String from = "aferditabeqiraj@gmail.com";  
+        String host = "localhost"; // or IP address 
+  
+        
+        Properties properties = System.getProperties();  
+  
+        
+        properties.setProperty("mail.smtp.host", host);  
+  
+        
+        Session session = Session.getDefaultInstance(properties);  
+  
+       
+        try { 
+  
+             
+            MimeMessage message = new MimeMessage(session);  
+   
+            message.setFrom(new InternetAddress(from));  
+            message.addRecipient(Message.RecipientType.TO,  
+                                new InternetAddress(to)); 
+            message.setSubject("subject"); 
+            message.setText("Hello"); 
+  
+            // Send message 
+            Transport.send(message); 
+            System.out.println("Hi there"); 
+        } 
+        catch (MessagingException mex) { 
+            mex.printStackTrace(); 
+        } 
+    } 
+}
